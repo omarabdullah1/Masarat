97a6ef70900ac4b8fae90c9bcec605d171bde4ca
abdelrahmanNabil15 <naabdo198@gmail.com>
2024-11-12 17:08:01 +0200

    -added app color constant -add a extension to convert hex color to Color

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+
+import 'package:masarat/core/utils/hex_color.dart';
+
+
+class AppColors{
+  static Color primary = HexColor('#00A89D');
+  static Color hint    = Colors.white30;
+  static Color text100= HexColor('#17202A');
+  static Color background    = HexColor('#F8F9F9');
+  static Color background2   =HexColor('#1B2631');
+  static Color withe    = Colors.white;
+  static Color txtColor = HexColor('#272727');
+  static Color blue     = HexColor('#11A6A1');
+
+}
