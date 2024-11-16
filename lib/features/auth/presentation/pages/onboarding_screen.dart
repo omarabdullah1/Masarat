6e06dc341d6eabf1ca554b233c2c67a55133bf43
abdelrahmanNabil15 <naabdo198@gmail.com>
2024-11-16 13:56:33 +0200

    -added onboarding screen

--- lib/features/auth/presentation/pages/onboarding_screen.dart
+++ lib/features/auth/presentation/pages/onboarding_screen.dart
@@ -10,117 +10,116 @@ import 'package:masarat/core/widgets/custom_button.dart';
 class ProfessionalTracksApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return   Scaffold(
-        backgroundColor: const Color(0xFF00A7A0),
-        body: SafeArea(
-          child: Center(
-            child: Column(
-              children: [
-                  Gap(
-                  30.h,
-                ), 
-                SvgPicture.asset(AppImage.iconAppWithText,
-                  height:     60.h,),
-                Gap(  70.h),
-                Text(
-                  'أهلاً بك  !',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24.sp,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: Constants.fontName,
-                  ),
+    return Scaffold(
+      backgroundColor: const Color(0xFF00A7A0),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Gap(
+                30.h,
+              ),
+              SvgPicture.asset(
+                AppImage.iconAppWithText,
+                height: 60.h,
+              ),
+              Gap(70.h),
+              Text(
+                'أهلاً بك  !',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24.sp,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: Constants.fontName,
                 ),
-                Gap( 8.h),
-                Text(
-                  'في المسارات الاحترافية',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24.sp,
-                    fontFamily: Constants.fontName,
-                  ),
+              ),
+              Gap(8.h),
+              Text(
+                'في المسارات الاحترافية',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24.sp,
+                  fontFamily: Constants.fontName,
                 ),
-                Text(
-                  ' من فضلك قم باختيار طريقة الدخول',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18.sp,
-                    fontFamily: Constants.fontName,
-                  ),
+              ),
+              Text(
+                ' من فضلك قم باختيار طريقة الدخول',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18.sp,
+                  fontFamily: Constants.fontName,
                 ),
-                Gap( 40.h),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 32.0.w),
-                  child: Column(
-                    children: [
-                      CustomButton(
-                        labelText: 'مدرب',
-                        buttonColor: AppColors.primary,
-                        textColor: AppColors.withe,
-                        borderColor: AppColors.withe,
-                        textFontSize: 22.sp,
-                        borderWidth: 2.w,
-                        onTap: () {},
-                      ),
-                      Gap( 16.h),
-                      CustomButton(
-                        labelText: 'متدرب',
-                        buttonColor: AppColors.withe,
-                        textColor: AppColors.primary,
-                        onTap: () {},
-                        textFontSize: 22.sp,
-
-                      ),
-                      Gap( 16.h),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: Divider(
-                              color: AppColors.withe,
-                              thickness: 1.h,
-                              indent: 32.w,
-                              endIndent: 8.w,
-                            ),
+              ),
+              Gap(40.h),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 32.0.w),
+                child: Column(
+                  children: [
+                    CustomButton(
+                      labelText: 'مدرب',
+                      buttonColor: AppColors.primary,
+                      textColor: AppColors.withe,
+                      borderColor: AppColors.withe,
+                      textFontSize: 22.sp,
+                      borderWidth: 2.w,
+                      onTap: () {},
+                    ),
+                    Gap(16.h),
+                    CustomButton(
+                      labelText: 'متدرب',
+                      buttonColor: AppColors.withe,
+                      textColor: AppColors.primary,
+                      onTap: () {},
+                      textFontSize: 22.sp,
+                    ),
+                    Gap(16.h),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Divider(
+                            color: AppColors.withe,
+                            thickness: 1.h,
+                            indent: 32.w,
+                            endIndent: 8.w,
                           ),
-                          Text('أو',
-                              style: TextStyle(
-                                  color: AppColors.withe,
-                                  fontSize: 18.sp,
-                                  fontFamily: Constants.fontName,),),
-                          Expanded(
-                            child: Divider(
-                              color: AppColors.withe,
-                              thickness: 1.h,
-                              indent: 8.w,
-                              endIndent: 32.w,
-                            ),
+                        ),
+                        Text(
+                          'أو',
+                          style: TextStyle(
+                            color: AppColors.withe,
+                            fontSize: 18.sp,
+                            fontFamily: Constants.fontName,
                           ),
-                        ],
-                      ),
-                      Gap( 16.h),
-                      CustomButton(
-                        labelText: 'موظف',
-                        buttonColor: AppColors.withe,
-                        textColor: AppColors.gery200,
-                        borderColor: AppColors.gery200,
-
-                        borderWidth: 2.w,
-                        onTap: () {},
-                        textFontSize: 22.sp,
-
-                      ),
-                    ],
-                  ),
+                        ),
+                        Expanded(
+                          child: Divider(
+                            color: AppColors.withe,
+                            thickness: 1.h,
+                            indent: 8.w,
+                            endIndent: 32.w,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Gap(16.h),
+                    CustomButton(
+                      labelText: 'موظف',
+                      buttonColor: AppColors.withe,
+                      textColor: AppColors.gery200,
+                      borderColor: AppColors.gery200,
+                      borderWidth: 2.w,
+                      onTap: () {},
+                      textFontSize: 22.sp,
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
-
+      ),
     );
   }
-
-  
 }
