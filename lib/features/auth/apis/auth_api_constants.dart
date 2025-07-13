8f3b2d5ffc1bf3f7e1016e89b279e9274aca20bc
root <rootco.dev@gmail.com>
2025-07-13 16:31:43 +0300

    fix: update API endpoints to include versioning in authentication and instructor constants

--- lib/features/auth/apis/auth_api_constants.dart
+++ lib/features/auth/apis/auth_api_constants.dart
@@ -3,12 +3,13 @@ import 'package:masarat/core/config.dart';
 class AuthenticationApiConstants {
   static String get apiBaseUrl => Config.get('apiUrl') as String;
 
-  static const String login = 'api/auth/login';
-  static const String loginGoogle = 'api/auth/login-google';
-  static const String loginApple = 'api/auth/login-apple';
-  static const String createAccount = 'api/auth/register';
-  static const String uploadAcademicDegree = 'api/auth/upload-academic-degree';
-  static const String myProfile = 'api/auth/my-profile';
-  static const String delete = 'api/users/delete';
-  static const String forget = 'api/auth/forgot-password';
+  static const String login = 'api/v1/auth/login';
+  static const String loginGoogle = 'api/v1/auth/login-google';
+  static const String loginApple = 'api/v1/auth/login-apple';
+  static const String createAccount = 'api/v1/auth/register';
+  static const String uploadAcademicDegree =
+      'api/v1/auth/upload-academic-degree';
+  static const String myProfile = 'api/v1/auth/my-profile';
+  static const String delete = 'api/v1/users/delete';
+  static const String forget = 'api/v1/auth/forgot-password';
 }
--- lib/features/student/courses/apis/courses_api_constants.dart
+++ lib/features/student/courses/apis/courses_api_constants.dart
@@ -3,7 +3,7 @@ import 'package:masarat/core/config.dart';
 class CoursesApiConstants {
   static String get apiBaseUrl => Config.get('apiUrl') as String;
 
-  static const String createCourse = 'api/courses';
-  static const String categories = 'api/categories';
-  static const String addLesson = 'api/lessons';
+  static const String createCourse = 'api/v1/courses';
+  static const String categories = 'api/v1/categories';
+  static const String addLesson = 'api/v1/lessons';
 }
